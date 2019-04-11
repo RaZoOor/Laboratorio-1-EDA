3e0775b47a447c09ed1dccf20fc502da4a8beb58
Francisco Guajardo Villa <franciscoguajardovilla@MacBook-Air-de-Francisco.local>
2019-04-11 17:21:50 -0400

    Actualizacion

--- Determinante.c
+++ Determinante.c
@@ -1,20 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int obtainMatrixSize(FILE archivo)   //deseo parar el puntero hacia el archivo
+{
+  int matrixSize;
+
+  return matrixSize;
+}
 
 int main ()
 {
   FILE *matrixFile;
   int i, j, matrixSize;
-  double** matrix;
+  char aux[256];
+  int** matrix;
 
   matrixFile = fopen ( "matriz.in", "r" ); // read mode
 
-  fscanf ( matrixFile, "%d", &matrixSize ); // save matrix size from the file
+  fscanf ( matrixFile, "%s", aux ); // save matrix size from the file
+  matrixSize = atoi(aux);
 
-  matrix = malloc ( matrixSize * sizeof ( double * ) );
+  matrix = malloc ( matrixSize * sizeof ( int * ) );
 
   for ( i = 0; i < matrixSize; i++ )
-    matrix[i] = malloc ( 4 * sizeof ( double ) );
+    matrix[i] = malloc ( 4 * sizeof ( int ) );
 
   fclose ( matrixFile );
 
