3fee5aa38ac480c4cb2cbf0f76844b94dd928f22
Francisco Guajardo Villa <franciscoguajardovilla@MacBook-Air-de-Francisco.local>
2019-04-11 22:11:57 -0400

    correccion de errores

--- Determinante.c
+++ Determinante.c
@@ -18,11 +18,18 @@ int main ()
   FILE *matrixFile;
   int i, j, matrixSize;
   int** matrix;
-  char fileName;
+  char fileName[ 256 ];
 
   printf("Ingrese nombre del archivo contenedor de la matriz: "); scanf("%s", fileName);
 
   matrixFile = fopen ( fileName, "r" ); // read mode
+
+  if ( matrixFile == NULL)
+  {
+    perror ( "Error al intentar de abrir el archivo... " );
+    exit ( EXIT_FAILURE );
+  }
+
   matrixSize = obtainMatrixSize ( matrixFile );
 
   matrix = malloc ( matrixSize * sizeof ( int * ) );
